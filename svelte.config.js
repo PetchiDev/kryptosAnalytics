07d51d92b3fec96c4266d33a95398cbfef949d12
Petchiappan.P <Petchiappan.P@kryptosinfosys.com>
2025-09-03 19:11:11 +0530

    Remove problematic runtime specification from svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// Vercel adapter options
-			runtime: 'nodejs18.x'
-		})
+		adapter: adapter()
 	}
 };
 
